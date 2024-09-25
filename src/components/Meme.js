eebb14c02209c729e7435a88e79518c3597de068
natzxn <skrzypskanatalia@gmail.com>
2024-09-25 19:28:04 +0200

    Edit styles for text top and bottom

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -22,22 +22,29 @@ function Meme() {
         }));
     }
 
+    const handleChange = (e) => {
+      const {name, value} = e.target
+      setMeme(prevMeme => ({
+        ...prevMeme,
+        [name]: value
+      }))
+    }
 
   return (
     <main className="main">
       <div className="form">
         <label>Top Text
-          <input type="text" className="form-input" placeholder="Hello" />
+          <input type="text" className="form-input" placeholder="Hello" name="topText" value={meme.topText} onChange={handleChange}/>
         </label>
         <label>Bottom Text
-          <input type="text" className="form-input" placeholder="World" /> 
+          <input type="text" className="form-input" placeholder="World" name="bottomText" value={meme.bottomText} onChange={handleChange}/> 
         </label>
         <button className="btn" onClick={getMeme}>Get a new meme image!</button>
       </div>
       <div className="meme">
         <img src={meme.randomImage} alt="meme image" className="meme-img"/>
-        <h2 className="meme-text"></h2>
-        <h2 className="meme-text"></h2>
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   );
